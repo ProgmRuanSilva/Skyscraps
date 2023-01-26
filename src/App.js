187b9d128c29f97fb2ed87654b7811e0357ce900
Ruan Silva <progmruansilva@gmail.com>
2023-01-26 20:44:13 -0300

    Header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './components/Header/'
+import BurjKalifa from './components/BurjKalifa';
+
+import './App.css';
+
+function App() {
+  return (
+   <div>
+     <Header />
+     <BurjKalifa />
+   </div> 
+  );
+}
+
+export default App;
---
+++ src/components/BurjKalifa/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function BurjKalifa() {
+    return(
+        <div>
+            <h3>Opa</h3>
+        </div>)
+}
